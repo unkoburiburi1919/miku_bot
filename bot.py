f585f190e06cefebafbe86f628697c165c1025f6
unkoburiburi1919 <iidakakeru714@gmail.com>
2026-06-04 22:05:19 +0900

    Refactor bot.py to print tweet instead of posting
    Removed Twitter API client code and added print statement for tweet.

--- bot.py
+++ bot.py
@@ -1,15 +1,7 @@
-import os
 import random
 
 words = ["ミク", "胸", "パッド"]
 
 tweet = f"{random.choice(words)}の{random.choice(words)}は{random.choice(words)}入り"
 
-client = tweepy.Client(
-    consumer_key=os.environ["API_KEY"],
-    consumer_secret=os.environ["API_SECRET"],
-    access_token=os.environ["ACCESS_TOKEN"],
-    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
-)
-
-client.create_tweet(text=tweet)
+print(tweet)
