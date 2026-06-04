cc0dcae80eeafcfbd5754276b1dc82e2dc503b36
unkoburiburi1919 <iidakakeru714@gmail.com>
2026-06-04 22:59:26 +0900

    Refactor Twitter client authentication and tweet creation

--- bot.py
+++ bot.py
@@ -1,18 +1,15 @@
 import os
-import random
 import tweepy
 
-words = ["ミク", "胸", "パッド"]
-
-tweet = f"{random.choice(words)}の{random.choice(words)}は{random.choice(words)}入り"
-
-client = tweepy.Client(
-    consumer_key=os.environ["API_KEY"],
-    consumer_secret=os.environ["API_SECRET"],
-    access_token=os.environ["ACCESS_TOKEN"],
-    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+auth = tweepy.OAuth1UserHandler(
+    os.environ["API_KEY"],
+    os.environ["API_SECRET"],
+    os.environ["ACCESS_TOKEN"],
+    os.environ["ACCESS_TOKEN_SECRET"]
 )
 
-client.create_tweet(text=tweet)
+api = tweepy.API(auth)
 
-print("投稿成功:", tweet)
+user = api.verify_credentials()
+
+print("認証成功:", user.screen_name)
