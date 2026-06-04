1f615de3209e8f6a6308328bce0dbedc494fd33e
unkoburiburi1919 <iidakakeru714@gmail.com>
2026-06-04 22:44:27 +0900

    Integrate Tweepy to post tweets with random words

--- bot.py
+++ bot.py
@@ -1,7 +1,18 @@
+import os
 import random
+import tweepy
 
 words = ["ミク", "胸", "パッド"]
 
 tweet = f"{random.choice(words)}の{random.choice(words)}は{random.choice(words)}入り"
 
-print(tweet)
+client = tweepy.Client(
+    consumer_key=os.environ["API_KEY"],
+    consumer_secret=os.environ["API_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+)
+
+client.create_tweet(text=tweet)
+
+print("投稿成功:", tweet)
