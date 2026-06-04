ae88d5f8ca7da269865363de787a2f98181b3c6b
unkoburiburi1919 <iidakakeru714@gmail.com>
2026-06-04 21:50:49 +0900

    Integrate Tweepy to post tweets automatically

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
+import os
 import random
+import tweepy
 
 words = ["ミク", "胸", "パッド"]
 
 tweet = f"{random.choice(words)}の{random.choice(words)}は{random.choice(words)}入り"
 
-print(tweet)
+client = tweepy.Client(
+    consumer_key=os.environ["API_KEY"],
+    consumer_secret=os.environ["API_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+)
+
+client.create_tweet(text=tweet)
